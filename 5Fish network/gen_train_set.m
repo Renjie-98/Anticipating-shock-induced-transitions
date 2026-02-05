ac7e5b9a3eeb82e62aac904be1795d0a74228081
Renjie-98 <renjiexu123@gmail.com>
2026-02-05 12:33:12 +0100

    Update gen_train_set.m

--- 5Fish network/gen_train_set.m
+++ 5Fish network/gen_train_set.m
@@ -8,48 +8,59 @@ function Y = gen_train_set()
 %  INPUT:
 %    • Required files (same folder):
 %        X_obs.mat    → Baseline node state (first observation)
-%        ratios.mat   → State decline ratios
+%        ratios.mat   → node loss
 %
 %  OUTPUT:
 %    • train_set.mat → 3D tensor (nTrials × nRatios × N)
-%        - Simulated node states under 
-%           state reduction
+%        - Simulated node loss
 % ============================================================
 clc;
 fprintf('\n=== Generating fish training set (state decline) ===\n');
-
-nTrials      = 1000;               
+nTrials      = 100000;               
 outFile      = 'train_set.mat';  
 
 %% ------------ Default settings ------------
+Tfile        = 'T.mat';           load(Tfile, 'T');                
 activityFile = 'X_obs.mat';       load(activityFile, 'X_obs');    
 ratioFile    = 'ratios.mat';      load(ratioFile, 'ratios');   
 % --- Extract variables ---
-stateRatios = ratios(:,1);          % State reduction ratios
-nRatios     = size(ratios,1);
+nodeRatios   = ratios(:,1);          % Node loss
+nRatios      = size(ratios,1);
 
-%% ------------ Initialize baseline state ------------
+% ------------ Basic sizes & baseline ------------
+T_base = double(T);                 %  transition matrix
+N = size(T_base, 1);
 x0 = double(X_obs(1,:));         % Baseline node state (first observation)
-N  = size(X_obs,2);              % Number of nodes
 
 %% ------------ Parallel perturbation loop ------------
-Y = zeros(nTrials, nRatios, N, 'double');
+Y = zeros(nTrials, N, nRatios,'double');
 parfor r = 1:nRatios
-    stateLoss  = stateRatios(r);    % Fractional reduction in overall node states
-   fprintf('→ Ratio %3d / %3d | State loss: %.3f\n', ...
-        r, nRatios, stateLoss);
+    p  = nodeRatios(r);    % node loss
+   
+    fprintf('→ Ratio %3d / %3d | p: %.3f\n', ...
+        r, nRatios, p);
+
     localY = zeros(nTrials, N);
-    for t = 1:nTrials
+   for t = 1:nTrials
+    % --- 1) random node removal ---
+    nRem   = round(p * N);
+    remIdx = randperm(N, nRem);
 
-        % State reduction ---
-        scaleFactor = 2 * (1 - stateLoss) * rand(1, N);  % 1×N vector
-        xPert = x0 .* scaleFactor;
-        % Record perturbed node states
-        localY(t,:) = xPert;
-    end
+    % --- 2) build perturbed transition matrix ---
+    Tpert = T;                     % copy baseline T
+    Tpert(remIdx,:) = 0;           % remove outgoing edges
+    Tpert(:,remIdx) = 0;           % remove incoming edges
 
-    Y(:,r,:) = localY;               
+    % --- 3) propagate ---
+    localY(t,:) = x0 * Tpert;
+         
+   end
+
+    Y(:, :, r) = localY;
 end
+
+Y = permute(Y, [1 3 2]);           % Convert to (nTrials × nRatios × N)
+
 
 %% ------------ Post-processing and save ------------
 for t = 1:nTrials
@@ -58,4 +69,5 @@ end
 
 save(outFile, 'Y', '-v7.3');        
 end
+
 
