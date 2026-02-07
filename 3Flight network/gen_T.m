6c2f4d718e5c62c7ec927d219589ab712eb51656
Renjie-98 <renjiexu123@gmail.com>
2026-02-07 16:11:07 +0100

    Update gen_T.m

--- 3Flight network/gen_T.m
+++ 3Flight network/gen_T.m
@@ -11,9 +11,9 @@ function generate_T()
 %        9.Passenger_flight network
 %
 %  OUTPUT (saved in current script folder):
-%    • T.mat        → Row-normalized transition matrix (baseline day)
-%    • activity.mat  → Airport outbound activity (F × N)
-%    • ratios.mat    → Directed link removal ratios (F × 1)
+%    • T.mat        →  Row-normalized transition matrix (baseline day)
+%    • X_obs.mat  →    Network activity (F × N)
+%    • ratios.mat    → Link removal ratios (F × 1)
 %
 clc;
 dataDir = 'C:\Users\xuren\Desktop\NP-real\0Data\9.Passenger_flight';  % Input folder (CSV data)
@@ -88,4 +88,5 @@ ratios = (linkCounts(1) - linkCounts) / linkCounts(1);
 save(fullfile(outDir, 'ratios.mat'), 'ratios', '-v7.3');
 
 end
+
 
